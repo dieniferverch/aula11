a9240fa3d5948690366227b8294f5abf3cbfd438
dieniferverch <142456047+dieniferverch@users.noreply.github.com>
2023-09-01 21:38:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+// DIENIFER VERCH
+
+
+//function imprimirOlaMundo() {
+//console.log("Olá Mundo");
+//}
+
+//imprimirOlaMundo();
+
+//function calculaArea(altura, largura){
+//const area = altura * largura;
+//console.log(area)
+//}
+
+//calculaArea:(2, 3);
+
+//function imprimirNome(nome) {
+
+//console.log("olá" + nome);
+//}
+//imprimirNome("Dienifer");
+
+//imprimirNome("vanessa");
+//imprimirNome("João");
+//imprimirNome("Marina");
+
+//let lernome = prompt("qual seu nome?");
+//imprimirNome(lernome);
+
+//const a = 1
+//function imprimeVariavel () {
+//  const b = 2
+//console.log(`variavel a`, a)
+//console.log(`variavel b`, b)
+//}
+//imprimeVariavel()
+
+//console.log(`variavel a`, a)
+//console.log(`variavel b`, b)
+
+//function calculaArea(altura, largura){
+//  const area = altura * largura
+//  return area
+//}
+
+//const areacalculada= calculaArea(2, 3)
+//console.log(calculaArea(2, 3))
+
+
+//function calculaArea(altura, largura){
+//  const area = altura * largura
+//  return area
+//}
+
+//const areacalculada= calculaArea(5, 8)
+//console.log(calculaArea(5, 8))
+
+
+//const a = 5
+//function imprimeVariavel () {
+//  const b = 6
+//console.log(`variavel a`, a)
+//console.log(`variavel b`, b)
+//}
+//imprimeVariavel()
+
+
+
+function array(){
+  const divisao3 = listaNumeros[listaNumeros.length -1] / 2;
+  const divisao0 = listaNumeros[listaNumeros.length -4] / 2;
+  return novoArray = [divisao3, divisao0];
+}
+let listaNumeros = [8, 6, 3, 12];
+console.log(array(listaNumeros));
